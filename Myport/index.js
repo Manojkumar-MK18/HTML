22d07bffb10161a5bebe29aed06974e81677a2e4
MK <manojkumar.sekar2000@gmail.com>
2023-02-20 11:14:13 +0530

    add

--- Myport/index.js
+++ Myport/index.js
@@ -1,15 +1,25 @@
 const header = document.querySelector("header");
 const icon = document.getElementById("top");
 const arrowIcon = document.getElementById("chedown");
-console.log(arrowIcon);
+
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 0); 
-  const scroolBolean = window.scrollY >= 80 ? true : false;
-  if (scroolBolean) {
-    icon.style.display = "flex";
-    arrowIcon.style.display = "flex";
-  } else {
-    icon.style.display = "none";
-    arrowIcon.style.display = "none";
-  }
+  // const scroolBolean = window.scrollY >= 80 ? true : false;
+  // if (scroolBolean) {
+  //   icon.style.display = "flex";
+  //   arrowIcon.style.display = "flex";
+  // } else {
+  //   icon.style.display = "none";
+  //   arrowIcon.style.display = "none";
+  // }
 });
+
+const onScroll = (event) => {
+  const scrollPosition = event.target.scrollingElement.scrollTop;
+  if (scrollPosition > 10) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+document.addEventListener("scroll", onScroll);
